1bc800d7acee448e52ecf7cddcb1b8163d60f86d
Ndcas <ndhung9911@gmail.com>
2024-10-03 16:47:56 +0700

    can giua

--- src/bookshelfapp/BookshelfApp.java
+++ src/bookshelfapp/BookshelfApp.java
@@ -1,8 +1,6 @@
 package bookshelfapp;
 
 import java.awt.CardLayout;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import panels.Login;
@@ -14,10 +12,8 @@ public class BookshelfApp extends JFrame {
     public BookshelfApp() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Bookshelf");
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (screenSize.width - 720) / 2;
-        int y = (screenSize.height - 480) / 2;
-        setBounds(x, y, 720, 480);
+        setSize(720, 480);
+        setLocationRelativeTo(null);
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
         //Thêm các panel ở đây
